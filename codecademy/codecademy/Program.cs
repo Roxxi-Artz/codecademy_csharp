00c8041edaa27d2c765bd2936a178df9463bb484
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2022-08-30 09:59:53 -0400

    Add files via upload

---
+++ codecademy/codecademy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            //this asks u how old u are lmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmao
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+        }
+    }
+}
+
